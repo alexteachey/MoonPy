fixed TOI masking issue

_mp_manipulation.py
@@ -460,6 +460,7 @@ def detrend(self, dmeth='cofiam', save_lc='y', mask_transits='y', period=None, m
 		self.mask_transit_idxs = np.array(all_quarter_mask_transit_idxs, dtype=object)					
 
 
+
 	else:
 		#### FOR ALL OTHER METHODS -- DETREND ON A QUARTER BY QUARTER BASIS.
 
@@ -497,7 +498,7 @@ def detrend(self, dmeth='cofiam', save_lc='y', mask_transits='y', period=None, m
 				### find out which transit midtimes, if any, are in this quarter
 				mask_transit_idxs = []
 
-				if use_holczer == 'y':
+				if (self.telescope.lower() == 'kepler') and (use_holczer == 'y'):
 
 					print("Using TTV Catalog to identify transit times for detrending...")
 					### taus should be calculated based on the Mazeh table.
@@ -596,10 +597,14 @@ def detrend(self, dmeth='cofiam', save_lc='y', mask_transits='y', period=None, m
 
 
 
-				elif use_holczer == 'n':
+				##### WE CAN ONLY USE HOLCZER FOR KEPLER PLANETS! SO IF IT AIN'T KEPLER...
+				else:
 					self.mask_taus = self.taus 
 
 
+
+
+				##### NOW THIS APPLIES EVERWHERE!
 				quarter_transit_taus = self.mask_taus[((self.mask_taus > np.nanmin(dtimes)) & (self.mask_taus < np.nanmax(dtimes)))]
 				try:
 					quarter_transit_taus = np.concatenate(quarter_transit_taus)
@@ -610,7 +615,27 @@ def detrend(self, dmeth='cofiam', save_lc='y', mask_transits='y', period=None, m
 				for qtt in quarter_transit_taus:
 					#### FOR EACH TRANSIT IN THIS QUARTER.... 
 
-					in_transit_idxs = np.where((dtimes >= float(qtt) - (self.mask_multiple / 2)*self.duration_days) & (dtimes <= float(qtt) + (self.mask_multiple / 2)*self.duration_days))[0]
+					if (use_holczer == 'y') and (self.telescope.lower() == 'kepler'):
+						in_transit_idxs = np.where( (dtimes >= float(qtt) - (5*self.duration_days)) & (dtimes <= float(qtt) + (5*self.duration_days)))[0]
+					
+
+					elif (use_holczer != 'y') or (self.telescope.lower() != 'kepler'):
+						#### NEW -- MARCH 2022 -- re-center the mask on the flux minimum!
+						in_transit_idxs = np.where( (dtimes >= float(qtt) - (self.mask_multiple/2) *self.duration_days) & (dtimes <= float(qtt) + (self.mask_multiple/2)*self.duration_days))[0]						
+						in_transit_flux_minimum_idx = np.nanargmin(dfluxes[in_transit_idxs])
+						new_in_transit_center_idx = in_transit_idxs[in_transit_flux_minimum_idx]
+						new_in_transit_center_time = dtimes[new_in_transit_center_idx]
+
+						#### update the self.taus value!
+						self_tau_idx = np.where(qtt == self.taus)[0]
+						self.taus[self_tau_idx] = new_in_transit_center_time
+						print('UPDATED TAU!')
+						print('was: '+str(qtt)+'; now: '+str(new_in_transit_center_time))
+
+						#### now update it
+						in_transit_idxs = np.where((dtimes >= float(new_in_transit_center_time) - (self.mask_multiple / 4)*self.duration_days) & (dtimes <= float(new_in_transit_center_time) + (self.mask_multiple / 4)*self.duration_days))[0]
+
+
 					mask_transit_idxs.append(in_transit_idxs)
 				
 				try:
@@ -620,20 +645,15 @@ def detrend(self, dmeth='cofiam', save_lc='y', mask_transits='y', period=None, m
 					print('mask_transit_idxs could not be concatenated (probably not needed).')
 
 
-				#print("BEFORE: ")
-				#print('mask_transit_idxs = ', mask_transit_idxs)
-				#print("mask transit times = ", dtimes[mask_transit_idxs])
-				#mask_transit_idxs = mask_transit_idxs
 
 				### add neighbor transit times to mask_transit_idxs.
 				try:
 					sntt = self.neighbor_transit_times # don't need to print them, just see if they're there!
 				except:
 					pass
-					#try:
-					#	self.mystery_solver(self.tau0, self.period, self.duration_hours)
-					#except:
-					#	pass
+
+
+
 
 				if (mask_neighbors == 'y') and (len(self.neighbors) > 0):
 
@@ -652,15 +672,13 @@ def detrend(self, dmeth='cofiam', save_lc='y', mask_transits='y', period=None, m
 							n_neighbor_idxs += 1
 						print('appended '+str(n_neighbor_idxs)+' neighbor transit data points for masking.')	
 
-				#try:
 
-				#print('mask_transit_idxs: ', mask_transit_idxs)
+
 
 				print('min, max quarter times: ', np.nanmin(dtimes), np.nanmax(dtimes))
 				
 				if len(quarter_transit_taus) > 0:
 					print(str(len(quarter_transit_taus))+" transit(s) in this quarter.")
-
 
 				if len(quarter_transit_taus) == 1:
 					try:
@@ -708,17 +726,18 @@ def detrend(self, dmeth='cofiam', save_lc='y', mask_transits='y', period=None, m
 				mask_transit_idxs = np.array([])
 
 			print(' ')
-			#print("AFTER: ")
-			#print('mask_transit_idxs = ', mask_transit_idxs)
 			if len(mask_transit_idxs) > 0:
-				print('len(mask_transit_idxs) = '+str(len(mask_transit_idxs)))
-				#print("mask transit times = ", dtimes[mask_transit_idxs])			
+				print('len(mask_transit_idxs) = '+str(len(mask_transit_idxs)))	
 
 
 			all_quarter_mask_transit_idxs.append(mask_transit_idxs)
 
 			##### update!
 			self.mask_transit_idxs = np.array(all_quarter_mask_transit_idxs, dtype=object)
+
+
+
+
 
 
 			if skip_quarter == 'n':

_mp_planet_fitter.py
@@ -473,20 +473,18 @@ def run_planet_fit(self, period=None, tau0=None, tdur_hours=None, smass=None, sm
 			# dataset --- HOW DOES THIS WORK
 			#map_soln = pmx.optimize(start=model.test_point)
 
-			for i in np.arange(0,5,1):
-				print("ITERATION "+str(i))
-				map_soln = pmx.optimize(start=model.test_point, vars=[log_sigma_lc, log_sigma_gp, log_rho_gp])
-				map_soln = pmx.optimize(start=map_soln, vars=[log_depth])
-				map_soln = pmx.optimize(start=map_soln, vars=[impact])
-				map_soln = pmx.optimize(start=map_soln, vars=[log10Period, t0])
-				map_soln = pmx.optimize(start=map_soln, vars=(log10Period, t0))
-				map_soln = pmx.optimize(start=map_soln, vars=[ldcs])
-				map_soln = pmx.optimize(start=map_soln, vars=[log_depth]) ### huh? why again?
-				map_soln = pmx.optimize(start=map_soln, vars=[impact]) #### why again?
-				map_soln = pmx.optimize(start=map_soln, vars=[ecs])
-				map_soln = pmx.optimize(start=map_soln, vars=[mean])
-				map_soln = pmx.optimize(start=map_soln, vars=[log_sigma_lc, log_sigma_gp, log_rho_gp]) #huh?
-				map_soln = pmx.optimize(start=map_soln) 
+			map_soln = pmx.optimize(start=model.test_point, vars=[log_sigma_lc, log_sigma_gp, log_rho_gp])
+			map_soln = pmx.optimize(start=map_soln, vars=[log_depth])
+			map_soln = pmx.optimize(start=map_soln, vars=[impact])
+			map_soln = pmx.optimize(start=map_soln, vars=[log10Period, t0])
+			map_soln = pmx.optimize(start=map_soln, vars=(log10Period, t0))
+			map_soln = pmx.optimize(start=map_soln, vars=[ldcs])
+			map_soln = pmx.optimize(start=map_soln, vars=[log_depth]) ### huh? why again?
+			map_soln = pmx.optimize(start=map_soln, vars=[impact]) #### why again?
+			map_soln = pmx.optimize(start=map_soln, vars=[ecs])
+			map_soln = pmx.optimize(start=map_soln, vars=[mean])
+			map_soln = pmx.optimize(start=map_soln, vars=[log_sigma_lc, log_sigma_gp, log_rho_gp]) #huh?
+			map_soln = pmx.optimize(start=map_soln) 
 			
 
 			extras = dict(zip(["light_curves", "gp_pred"], pmx.eval_in_model([light_curves, gp.predict(residuals)], map_soln),))

moonpy.py
@@ -99,7 +99,7 @@ class MoonpyLC(object):
 	### when you initialize it, you'll either give it the times, fluxes, and errors, OR
 	### you'll provide a targetID and telescope, which will allow you to download the dataset!
 
-	def __init__(self, targetID=None, target_type=None, lc_times=None, lc_fluxes=None, lc_errors=None, lc_flags=None, lc_quarters=None, usr_dict=None, mask_multiple=4, quarters='all', telescope=None, RA=None, Dec=None, coord_format='degrees', search_radius=5, lc_format='pdc', remove_flagged='y', short_cadence=False, ffi='n', save_lc='y', load_lc='n', download='y', is_neighbor='n', attributes_only='n', clobber=None):
+	def __init__(self, targetID=None, target_type=None, lc_times=None, lc_fluxes=None, lc_errors=None, lc_flags=None, lc_quarters=None, usr_dict=None, mask_multiple=20, quarters='all', telescope=None, RA=None, Dec=None, coord_format='degrees', search_radius=5, lc_format='pdc', remove_flagged='y', short_cadence=False, ffi='n', save_lc='y', load_lc='n', download='y', is_neighbor='n', attributes_only='n', clobber=None):
 		
 		global first_kepler, first_k2, first_tess, first_koi			
 		global kepler_NEA_data, kepler_NEA_columns, kepler_exofop_data, kepler_exofop_columns 
@@ -1039,6 +1039,10 @@ class MoonpyLC(object):
 				neighbor_taus = neighbor_dict[neighbor].taus
 			neighbor_dur = neighbor_dict[neighbor].duration_days 
 
+			self.neighbor_taus = neighbor_taus
+
+			self.all_taus = np.concatenate((self.taus, self.neighbor_taus))
+
 			print('appending '+str(len(neighbor_taus))+' neighbor transit times.')
 			print('neighbor duration = '+str(neighbor_dur))
 
@@ -1054,11 +1058,14 @@ class MoonpyLC(object):
 		except:
 			print('self.taus not available. Setting target_taus = np.array([np.nan])')
 			target_taus = np.array([])
+
 		try:
 			target_dur = self.duration_days
 		except:
 			print('self.duration_days not available. Setting target_dur = np.nan')
 			target_dur = np.nan 
+
+
 		for tt in target_taus:
 			ttidxs = np.where((np.hstack(self.times) >= (tt - (self.mask_multiple/2)*target_dur)) & (np.hstack(self.times) <= (tt + (self.mask_multiple/2)*target_dur)))[0]
 			for ttidx in ttidxs:

mp_detrend.py
@@ -129,10 +129,12 @@ def cofiam_detrend(times, fluxes, errors, telescope='kepler', remove_outliers='y
 		### you need to detrend the two halves separately!
 		deltats = []
 		for nut, ut in enumerate(unmasked_times):
-			if nut == unmasked_times.size-1:
+			#if nut == unmasked_times.size-1:
+			#	pass
+			if nut == 0:
 				pass
 			else:
-				deltats.append(unmasked_times[nut+1] - unmasked_times[nut])
+				deltats.append(unmasked_times[nut] - unmasked_times[nut-1])
 
 		deltats = np.array(deltats)
 		deltat = np.nanmedian(deltats)
@@ -218,10 +220,12 @@ def polyAM_detrend(times, fluxes, errors, telescope=None, remove_outliers='y', o
 		### you need to detrend the two halves separately!
 		deltats = []
 		for nut, ut in enumerate(unmasked_times):
-			if nut == unmasked_times.size-1:
+			#if nut == unmasked_times.size-1:
+			#	pass
+			if nut == 0:
 				pass
 			else:
-				deltats.append(unmasked_times[nut+1] - unmasked_times[nut])
+				deltats.append(unmasked_times[nut] - unmasked_times[nut-1])
 
 		deltats = np.array(deltats)
 		deltat = np.nanmedian(deltats)
@@ -323,10 +327,12 @@ def polyLOC_detrend(times, fluxes, errors, telescope=None, remove_outliers='y', 
 		### you need to detrend the two halves separately!
 		deltats = []
 		for nut, ut in enumerate(unmasked_times):
-			if nut == unmasked_times.size-1:
+			#if nut == unmasked_times.size-1:
+			#	pass
+			if nut == 0:
 				pass
 			else:
-				deltats.append(unmasked_times[nut+1] - unmasked_times[nut])
+				deltats.append(unmasked_times[nut] - unmasked_times[nut-1])
 		deltats = np.array(deltats)
 		deltat = np.nanmedian(deltats)
 