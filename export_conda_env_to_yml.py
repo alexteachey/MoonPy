80141c22f7e5963a45cc5b6dc5426f56cde382bc
Alex Teachey <ateachey@gmail.com>
2022-08-10 14:36:51 +0800

    name is updated in the file

--- export_conda_env_to_yml.py
+++ export_conda_env_to_yml.py
@@ -5,7 +5,7 @@ current_environment = os.environ['CONDA_DEFAULT_ENV']
 
 ### export this environment to a .yml file
 
-output_name = input("What name do you want to give the output environment file? Press ENTER to keep it the same as the current environment.")
+output_name = input("What name do you want to give the output environment file? (Press ENTER to keep it the same as the current environment): ")
 
 if output_name == '':
 	output_name = current_environment
@@ -14,3 +14,21 @@ export_command = 'conda env export -n '+current_environment+' -f '+output_name+'
 
 os.system(export_command)
 
+
+
+#### now make sure you replace the current_environment name with the output name!
+exported_filename = output_name+'.yml'
+replacement_filename = 'replacement_'+output_name+'.yml'
+
+orig_file = open(exported_filename, mode='r')
+new_file = open(replacement_filename, mode='w')
+
+for nline,line in enumerate(orig_file):
+	line = line.replace(current_environment, output_name)
+	new_file.write(line)
+
+orig_file.close()
+new_file.close()
+
+### replace the original file with the new file
+os.system('mv '+replacement_filename+' '+exported_filename)
